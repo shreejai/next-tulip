b798bed5aa9cda3c74560091ff2e75e1af1df526
shreejai <shreejai@icloud.com>
2024-03-31 15:18:15 +1100

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar1 from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className="text-danger">Hello Bootstrap</h1>
+      <Navbar1/>
     </main>
   );
 }
